2ef96eb4f6ad513ffe1e959dfa61285091529311
Crowdbotics <team@crowdbotics.com>
2024-02-22 05:22:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pando-supply-46558",
+  url: "https://pando-supply-46558.botics.co",
+  api: "https://pando-supply-46558.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
